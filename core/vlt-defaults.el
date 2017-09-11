2d7b6481ed848a8e92e92ea49ac151e6362a4a6c
Daniel Barreto <daniel.barreto.n@gmail.com>
2017-09-11 02:10:24 +0200

    Colorize compilation buffer

--- core/vlt-defaults.el
+++ core/vlt-defaults.el
@@ -207,7 +207,14 @@
 
 ;; Colorize output of Compilation Mode, see
 ;; http://stackoverflow.com/a/3072831/355252
-(add-hook 'compilation-filter-hook #'prelude-colorize-compilation-buffer)
+(defun vlt-colorize-compilation-buffer ()
+  "Colorize a compilation mode buffer."
+  (interactive)
+  ;; we don't want to mess with child modes such as grep-mode, ack, ag, etc
+  (when (eq major-mode 'compilation-mode)
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region (point-min) (point-max)))))
+(add-hook 'compilation-filter-hook #'vlt-colorize-compilation-buffer)
 
 ;; zap-up-to-char
 (autoload 'zap-up-to-char "misc"
