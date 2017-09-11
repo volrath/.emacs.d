d1e5306c81a74db474fed19cc0f5a1af8834b3df
Daniel Barreto <daniel.barreto.n@gmail.com>
2017-09-12 01:41:09 +0200

    Add a way to extend theme face customizations

--- appearance/vlt-theme-helpers.el
+++ appearance/vlt-theme-helpers.el
@@ -18,12 +18,23 @@
 (defun vlt-solarized-theme-p (theme-name)
   (string-prefix-p "solarized" (symbol-name theme-name)))
 
+(defun vlt-extend-theme-customization (theme)
+  (let* ((class '((class color) (min-colors 89)))
+         (faces
+          `((solarized-light
+             (linum ((,class (:weight normal :underline nil :foreground "#93a1a1" :background "#fdf6e3"))))
+             (fringe ((,class (:background "#fcf1d5")))))
+            (solarized-dark
+             (linum ((,class (:weight normal :underline nil :foreground "#586e75" :background "#002b36"))))
+             (fringe ((,class (:background "#002630"))))))))
+    (apply #'custom-theme-set-faces theme (alist-get theme faces nil))))
+
 (defun vlt-enable-theme-with-font (theme font)
   (dolist (theme-pair vlt-themes)
     (disable-theme (car theme-pair)))
   (load-theme theme)
-  (vlt-setup-mode-line-format theme)
-  )
+  (vlt-extend-theme-customization theme)
+  (vlt-setup-mode-line-format theme))
 
 (defun vlt-enable-current-theme ()
   (let* ((theme-pair (nth vlt-current-theme-index vlt-themes))
