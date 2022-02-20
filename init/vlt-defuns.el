3a7d6fd49dc815c6d10726b626a18ace442d4bee
Daniel Barreto <daniel@barreto.tech>
2022-02-20 02:35:14 +0100

    Add some extra defuns I forgot from Charlotte

--- init/vlt-defuns.el
+++ init/vlt-defuns.el
@@ -9,7 +9,7 @@
 ;;
 ;;; Commentary:
 ;;
-;;
+;; Mostly copied from Magnars :)
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -35,7 +35,6 @@
 
 ;;; Buffer-related defuns
 ;;  ----------------------------------------------------------------------------
-;; Mostly copied from Magnars :)
 
 ;;;###autoload
 (defun create-scratch-buffer nil
@@ -132,6 +131,44 @@ Including indent-buffer, which should not be called automatically on save."
 
 
 
+;;; Files
+;;  ----------------------------------------------------------------------------
+
+;;;###autoload
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+
+;;;###autoload
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+
+
 ;;; Misc
 ;;  ----------------------------------------------------------------------------
 
@@ -193,10 +230,6 @@ If there's no region, the current line will be duplicated."
     (duplicate-current-line arg)
     (one-shot-keybinding "d" 'duplicate-current-line)))
 
-
-
-;;; Misc
-;;  ----------------------------------------------------------------------------
 
 (defun vlt/wrap-with (s)
   "Create a wrapper function for smartparens using S."
