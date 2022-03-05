5a5981f8ddc86d33f93f42b95cb31df55b1ccc6b
Daniel Barreto <daniel@barreto.tech>
2022-03-05 09:14:18 +0100

    Make sure to activate page-break-lines in clojure mode

--- packages/vlt-packages.el
+++ packages/vlt-packages.el
@@ -155,6 +155,7 @@ clean buffer we're an order of magnitude laxer about checking."
     (when (bound-and-true-p page-break-lines-mode)
       (move-beginning-of-line nil)))
   :config
+  (add-to-list 'page-break-lines-modes 'clojure-mode)
   (global-page-break-lines-mode)
   (advice-add 'backward-page :after #'vlt/page-navigation-move-to-bol)
   (advice-add 'forward-page :after #'vlt/page-navigation-move-to-bol))
