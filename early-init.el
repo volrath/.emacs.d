ce05cdd553747d11ff0298a600ccdd3ceb2e18b4
Daniel Barreto <daniel@barreto.tech>
2022-11-30 17:21:24 +0100

    Minor hack to avoid native-comp warnings in macos
    Fix for this has been supposedly been merged to `master` and will be released
    with version 29.

--- early-init.el
+++ early-init.el
@@ -1,6 +1,18 @@
-;; Disable package.el
-(setq package-enable-at-startup nil)
+;; Native Compilation
+;; -----------------------------------------------------------------------------
+;; Problems with native compilation in macos
+;; https://issues.guix.gnu.org/issue/57849
+(when (eq system-type 'darwin)
+  (customize-set-variable 'native-comp-driver-options '("-Wl,-w")))
 
+
+;; Package Manager
+;; -----------------------------------------------------------------------------
+(setq package-enable-at-startup nil)  ;; Disable package.el
+
+
+;; UI
+;; -----------------------------------------------------------------------------
 ;; Turn off mouse interface early in startup to avoid momentary display
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
