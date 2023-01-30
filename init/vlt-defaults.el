f78e27b9e91e7c63e3147858c07e50bc2d0e2ded
Daniel Barreto <daniel@barreto.tech>
2023-01-30 20:49:27 +0100

    Configure `tramp` access to `docker` containers

--- init/vlt-defaults.el
+++ init/vlt-defaults.el
@@ -183,6 +183,29 @@
                               (display-fill-column-indicator-mode t)))
 
 
+;; Tramp settings
+(push
+ (cons
+  "docker"
+  '((tramp-login-program "docker")
+    (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+    (tramp-remote-shell "/bin/sh")
+    (tramp-remote-shell-args ("-i") ("-c"))))
+ tramp-methods)
+
+(defadvice tramp-completion-handle-file-name-all-completions
+    (around dotemacs-completion-docker activate)
+  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
+    a list of active Docker container names, followed by colons."
+  (if (equal (ad-get-arg 1) "/docker:")
+      (let* ((dockernames-raw (shell-command-to-string "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
+             (dockernames (cl-remove-if-not
+                           #'(lambda (dockerline) (string-match ":$" dockerline))
+                           (split-string dockernames-raw "\n"))))
+        (setq ad-return-value dockernames))
+    ad-do-it))
+
+
 ;; Email
 (setq send-mail-function 'smtpmail-send-it
       smtpmail-smtp-service 587)  ;; server & user in private.el
